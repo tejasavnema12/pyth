c2e4d4fd8f971eadf1eaa8991d682f0426dc0d66
tejasavnema12 <122636499+tejasavnema12@users.noreply.github.com>
2023-01-24 00:03:14 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,24 @@
+#list
+list=["parrot","q","r"]
+print(list)
+
+
+#length
+print(len(list))
+
+
+#type of list
+print(type(list))
+
+         
+#access of list                                                   
+print(list[1])
+
+         
+#negative indexing                                                  
+print(list[-1])
+#output: r
+         
+#range of index                                                    
+print(list[1:3])     
+ 
