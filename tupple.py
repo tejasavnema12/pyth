e9cda293c8c5b4c35f666c92efd978f0b0141b95
tejasavnema12 <122636499+tejasavnema12@users.noreply.github.com>
2023-01-24 00:04:18 +0530

    Add files via upload

---
+++ tupple.py
@@ -0,0 +1,20 @@
+#tuples
+var = ("tejas", "hi", "python")
+ 
+print("Value in Var[0] = ", var[0])
+print("Value in Var[1] = ", var[1])
+print("Value in Var[2] = ", var[2])
+#negative indexing in tuples
+var = ("as", "by", "ty")
+ 
+print("Value in Var[-3] = ", var[-3])
+print("Value in Var[-2] = ", var[-2])
+print("Value in Var[-1] = ", var[-1])
+#slicing in python
+tuple1 = (0 ,1, 2, 3)
+print(tuple1[1:])
+print(tuple1[::-1])
+print(tuple1[2:4])
+#finding length of tuples
+tuple2 = ('python', 'tejas')
+print(len(tuple2))
